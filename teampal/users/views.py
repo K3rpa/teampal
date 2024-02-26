d06ba2faa39c5a37c3f1d3ea927ada16bc43b19b
K3rpa <k3rpa01@gmail.com>
2024-02-25 22:56:27 -0800

    update login page,dashboard; add feature logout in dashboard

--- teampal/users/views.py
+++ teampal/users/views.py
@@ -2,10 +2,17 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 
 def home(request):
-    return render(request,"home.html")
+    return render(request,"login.html")
 
 
 def logout_view(request):
     logout(request)
     return redirect("/")
+
+
+def dashboard(request):
+    if request.user.is_authenticated:
+        return render(request, "dashboard.html")
+    else:
+        return redirect("login.html")  
 
